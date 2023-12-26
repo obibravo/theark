3823d450802125630a9096c9643c9877aab2f7d8
Eze David Obinna <obibravo54@gmail.com>
2023-12-26 20:02:06 +0100

    origin

--- walletoverlap/views.py
+++ walletoverlap/views.py
@@ -209,7 +209,7 @@ def etherscan(request):
             token_name = wallet_address
             token_address =  tx['hash']
             token_balance =0
-            token_quantity =  float(tx['value']) / 1e18
+            token_quantity =  float(float(tx['value']) / 1e18)
 
             # Create and save Wallet instance
             print(wallet)
