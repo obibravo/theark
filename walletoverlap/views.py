3e5c45f66aabe3b4be73ec6935aba781502bb95e
Eze David Obinna <obibravo54@gmail.com>
2023-12-26 19:11:36 +0100

    origin

--- walletoverlap/views.py
+++ walletoverlap/views.py
@@ -2,7 +2,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from web3 import Web3
+# from web3 import Web3
 import json
 from .models import EthereumTransaction, Token, Wallet
 
